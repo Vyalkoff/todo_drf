2e7fbd439e9eaa0470241cb3ab5c1ebe6ec8486b
vlad <kingbingping@gmail.com>
2021-11-21 14:13:09 +0500

    add add frontend

--- todo/users/serializers.py
+++ todo/users/serializers.py
@@ -6,4 +6,4 @@ from .models import Users
 class UsersModelSerializer(ModelSerializer):
     class Meta:
         model = Users
-        fields = ('first_name', 'last_name', 'username', 'email',)
+        fields = ('first_name', 'last_name', 'username', 'email', 'url')
