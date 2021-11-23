23f5a62d40fdf6b4f1a3b2398aa881207e904d6e
vlad <kingbingping@gmail.com>
2021-11-23 21:38:45 +0500

    add menu and footer

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,40 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-import React  from "react";
+import React from "react";
 import axios from "axios";
 import UsersList from "./components/Users";
+import Navbar from './components/Menu';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from "./components/footer";
 
-class App extends React.Component{
-  constructor(props) {
-    super(props)
-      this.state = {
-        'users':[]
-      }
-  }
-
-  componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/users/').then(
-        response =>{
-            const users = response.data
-            this.setState(
-                {
-                    'users':users
-                }
-            )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
         }
-    ).catch(error => console.log(error))
-  }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users/').then(
+            response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            }
+        ).catch(error => console.log(error))
+    }
 
 
-  render(){
-    return (
-        <div>
-          <UsersList users={this.state.users}/>
-        </div>
+    render() {
+        return (
+            <div>
+            <div>
+                <Navbar/>
+            </div>
+                <div>
+                    <UsersList users={this.state.users}/>
+                </div>
+                <Footer/>
 
-  );
-  }
+            </div>
+
+        );
+    }
 
 }
+
 
 export default App;
